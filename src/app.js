be923bc968f2fb4ecb2fc09ef6552bda7884e854
Silvia <silviatmnez@gmail.com>
2021-10-07 22:17:19 +0000

    created random card generator

--- src/app.js
+++ src/app.js
@@ -4,8 +4,48 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { eventListeners } from "@popperjs/core";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const changeCard = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let suits = ["♦", "♥", "♠", "♣"];
+    let randomNumber = Math.floor(Math.random() * numbers.length);
+    let randomSuit = Math.floor(Math.random() * suits.length);
+    let number = document.querySelector(".number");
+    let suit = document.querySelector(".suit");
+    let suit2 = document.querySelector(".suit-upside-down");
+
+    if (randomSuit == 2 || randomSuit == 3) {
+      suit.style.color = "black";
+      suit2.style.color = "black";
+    } else {
+      suit.style.color = "red";
+      suit2.style.color = "red";
+    }
+
+    number.textContent = numbers[randomNumber];
+    suit.textContent = suits[randomSuit];
+    suit2.textContent = suits[randomSuit];
+  };
+  changeCard();
+
+  let button = document.querySelector(".btn");
+  button.addEventListener("click", changeCard);
 };
